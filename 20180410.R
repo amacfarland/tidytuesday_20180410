ccedf75eb0d30f28b9509a09f07feb12c5e24bd0
andrewmacfarland <andrew@ideacollective.org>
2018-04-10 15:58:34 -0600

    update code

--- 20180410.R
+++ 20180410.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(grid)
+library(gridExtra)
 
 df <- readxl::read_excel("dat/tidy_tuesday_week2.xlsx")
 
@@ -34,9 +34,19 @@ plot.offense <- df_top16 %>%
   scale_y_continuous(limits = c(0, 25)) +
   facet_wrap(~position, nrow = 1) +
   ggtitle("Offense") +
-  scale_x_date(date_breaks = "4 years", date_labels = paste0("'", "%y"))
-
-plot.offense
+  scale_x_date(date_breaks = "4 years", date_labels = paste0("'", "%y")) +
+  labs(
+    x = "Year",
+    y = "Salary (Millions of USD)"
+  ) +
+  theme(
+    panel.background = element_blank(),
+    panel.border = element_rect(fill = NA),
+    text = element_text(size = 10),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(color = "grey80"),
+    panel.grid.minor.y = element_blank()
+  )
 
 plot.defense <- df_top16 %>% 
   filter(as.integer(position) > 5) %>% 
@@ -46,7 +56,19 @@ plot.defense <- df_top16 %>%
   scale_y_continuous(limits = c(0, 25)) +
   facet_wrap(~position, nrow = 1) +
   ggtitle("Defense") +
-  scale_x_date(date_breaks = "4 years", date_labels = paste0("'", "%y"))
+  scale_x_date(date_breaks = "4 years", date_labels = paste0("'", "%y")) +
+  labs(
+    x = "Year",
+    y = "Salary (Millions of USD)"
+  ) +
+  theme(
+    panel.background = element_blank(),
+    panel.border = element_rect(fill = NA),
+    text = element_text(size = 10),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(color = "grey80"),
+    panel.grid.minor.y = element_blank()
+  )
 
 g2 <- ggplotGrob(plot.offense)
 g3 <- ggplotGrob(plot.defense)
@@ -54,3 +76,4 @@ g <- rbind(g2, g3, size = "first")
 g$widths <- unit.pmax(g2$widths, g3$widths)
 grid.newpage()
 grid.draw(g)
+grid.arrange(g, top=textGrob(paste0("Average cap value of 16 highest-paid NFL players in each position", "\n"), gp=gpar(fontsize=18,font=8)))
